89d0e9915c0475eb1c8e6167dc4ac87e540e2da9
John Baycroft <jbaycroft@capaccio.com>
2016-06-08 13:15:57 -0700

    saved ajax loader

--- react/src/main.js
+++ react/src/main.js
@@ -2,57 +2,52 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Category Component
-var MetricCat = React.createClass({
+// Datapoints Component
+var Datapoints = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
+    var datapointNodes = this.state.data.map(function(datapoint) {
+      return (
+        <Datapoint
+          key = {datapoint.id}
+          datatype={datapoint.datatype}
+          measuretype = {datapoint.measuretype}
+          usage = {datapoint.usage}
+          meter_reading = {datapoint.meter_reading}
+          />
+      )
+    })
     return (
-      <select>
-          <option>Water</option>
-          <option>Electricity</option>
-      </select>
+      <div>{datapointNodes}</div>
     );
   }
 });
-
-setInterval(function() {
-  ReactDOM.render(
-    <MetricCat  />,
-    document.getElementById('category')
-  );
-});
-
-//Type Component
-var MetricType = React.createClass({
+var Datapoint = React.createClass({
   render: function() {
     return (
-      <select>
-          <option>Usage</option>
-          <option>Meter Reading</option>
-      </select>
-    );
+      <li>{this.props.datatype}</li>
+    )
   }
-});
+})
 
 setInterval(function() {
   ReactDOM.render(
-    <MetricType  />,
-    document.getElementById('type')
+    <Datapoints url="api/v1/datapoints" />,
+    document.getElementById('datapoints')
   );
-});
-
-//Input Component
-
-var InputField = React.createClass({
-  render: function() {
-    return (
-      <input type="text" className="form-control" id="qty" placeholder="100 kWh"/>
-    );
-  }
-});
-
-setInterval(function() {
-  ReactDOM.render(
-    <InputField  />,
-    document.getElementById('inputdata')
-  );
-});
+},500);
