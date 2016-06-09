974842bc6dcef32f2919118e99d78cd8ed6796d0
CAP132-MAC <CAP132-MAC@CAP132-MACs-MacBook-Pro.local>
2016-06-09 13:40:50 -0400

    added route for datapoints index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ReactTest::Application.routes.draw do
   root "lists#index"
 
+  resources :datapoints, only: [:index]
+  
   namespace :api do
     namespace :v1 do
       resources :datapoints, only: [:index, :create, :destroy]
