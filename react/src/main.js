fe34e9ca63ddce9aa7a1736594c603e823181b62
Caitlin Turney <cturney@capaccio.com>
2016-06-10 13:37:27 -0400

    got form working need to build out logic

--- react/src/main.js
+++ react/src/main.js
@@ -5,31 +5,74 @@ var metricCats
 var MetricForm = React.createClass ({
   getInitialState: function() {
     return {
-      category: '',
-      type: '',
-      amount: '',
+      category: 'Air',
+      type: 'Usage',
+      amount: 'PlaceHolder',
     };
   },
-  handleChange: function(event) {
-    this.setState({value: event.target.value});
+  handleCategoryChange: function(event) {
+    this.setState({category: event.target.value});
+  },
+  handleTypeChange: function(event) {
+    this.setState({type: event.target.value});
+  },
+  handleInputChange: function(event) {
+    this.setState({amount: event.target.value});
   },
 
   render: function() {
 
     const categoryList = ['Air', 'Water', 'Electricity', 'Waste'];
+    const typeList = ['Usage', 'Meter Reading'];
 
     return (
-      <select>
-        {categoryList.map( category => <option>{category}</option>)}
-      </select>
+      <div>
+        <form>
+          <div className="form-group">
+            <label for ="metcat">Category </label>
+            <select onChange={this.handleCategoryChange}>
+              {categoryList.map( category => <option value={category}>{category}</option>)}
+            </select>
+          </div>
+          <div className="form-group">
+            <label for ="mettype">Type </label>
+            <select onChange={this.handleTypeChange}>
+              {typeList.map( type => <option value={type}>{type}</option>)}
+            </select>
+          </div>
+          <div className="form-group">
+            <label for ="metamt">Amount </label>
+            <input
+            type='text'
+            value={this.state.amount}
+            onChange={this.handleInputChange}
+            />
+          </div>
+      </form>
+      </div>
     )
   }
 
 });
-
+var FormBox = React.createClass ({
+    render: function () {
+      return (
+        <div>
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <h2>Data Form</h2>
+            </div>
+            <div className="panel-body">
+              <MetricForm />
+            </div>
+          </div>
+        </div>
+    )
+  }
+})
 setInterval(function() {
   ReactDOM.render(
-    <MetricForm />,
+    <FormBox />,
     document.getElementById('dataForm')
   );
 });
